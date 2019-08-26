802dfdf83d33a5b12ce32d6bf25abbc6b3f2e345
thanhhau097 <thanhhau097@gmail.com>
2019-08-26 20:30:35 +0700

    fix bug dataloader

--- data_loader/ocr_data_loader.py
+++ data_loader/ocr_data_loader.py
@@ -144,9 +144,9 @@ class OCRDataLoader(object):
                 else:  # joint: we need to return both output of CTC and attention
                     # TODO consider label_length here, because there is mix type, then label += '\n'
                     new_label_length = np.zeros((self.batch_size, 1))
-                    for i, label_l in enumerate(label_length):
+                    for k, label_l in enumerate(label_length):
                         if label_l == 1:
-                            new_label_length[i] = label_length - 1
+                            new_label_length[k] = label_length - 1
                     input_length = np.ones((self.batch_size, 1)) * (max_width // self.config.downsample_factor - 2)
 
                     decoder_input_data = np.zeros([self.batch_size, 1, int(self.config.n_letters)])
